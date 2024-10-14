8c9eb559c3aa3628d6afa3853030c97daaa3d2d7
and078 <andreynovikov078@gmail.com>
2024-10-14 12:22:18 +0100

    change post /device handler

--- api/src/index.js
+++ api/src/index.js
@@ -6,11 +6,6 @@ const morgan = require('morgan');
 
 const app = express();
 
-const ads = [
-    { title: 'Hello, world (again)!' }
-];
-
-
 // adding Helmet to enhance your API's security
 app.use(helmet());
 
@@ -25,32 +20,39 @@ app.use(morgan('combined'));
 
 const esps = {
     lolin: 'http://192.168.1.185:5555/test',
-    esp: 'http://192.168.1.184:80/test',
+    esp: 'http://192.168.1.184:5556/test',
 }
 
+const handleResponseFromEsp = (response, device, res) => {
+    res.send({
+        'status': response,
+        'device': device,
+    });
+}
 
-const sendPostRequest = async (localAddress, status) => {
+const sendPostRequest = async (localAddress, status, device, res) => {
+    try {
+        await fetch(localAddress, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ status: status })
+        }).then(response => response.json())
+          .then(response => handleResponseFromEsp(response, device, res))
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+app.get('/all-devices-status', (req, res) => {
     
-    await fetch(localAddress, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ status: status })
-    }).then(res => res.json())
-      .then(res => console.log(res))
-}
-
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send(ads);
 });
 
-app.post('/', (req, res) => {
-    let device = req.body.id;
-    sendPostRequest(esps[device], req.body.status);
-    res.send("ok");
+
+app.post('/device', (req, res) => {
+    sendPostRequest(esps[req.body.id], req.body.status, req.body.id, res);
 });
 
 app.listen(3001, () => {
