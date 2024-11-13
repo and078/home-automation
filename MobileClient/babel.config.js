243f0674c038c7f1bb07900364ab4c8c74a30090
and078 <andreynovikov078@gmail.com>
2024-11-13 21:59:19 +0000

    add .env.production to  .env works in production

--- MobileClient/babel.config.js
+++ MobileClient/babel.config.js
@@ -8,6 +8,8 @@ module.exports = function (api) {
         {
           moduleName: '@env',
           path: '.env',
+          "safe": true,
+          "allowUndefined": false
         },
       ],
     ],
